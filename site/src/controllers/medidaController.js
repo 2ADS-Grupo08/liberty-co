9461f0bff2b1dbdb3c0cd7907cc91eedf73b8b0f
MarianaNamie <mariana.ribeiro@sptech.school>
2023-05-23 21:47:29 -0300

    Início das rotas do Dashboard

--- site/src/controllers/medidaController.js
+++ site/src/controllers/medidaController.js
@@ -1,10 +1,10 @@
 var medidaModel = require("../models/medidaModel");
 
-function buscarUltimasMedidas(req, res) {
+function medidaIdealComponentes(req, res) {
 
     const limite_linhas = 7;
 
-    var idAquario = req.params.idAquario;
+    var idMaquina = req.params.idMaquina;
 
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
@@ -21,28 +21,49 @@ function buscarUltimasMedidas(req, res) {
     });
 }
 
+// function buscarUltimasMedidas(req, res) {
 
-function buscarMedidasEmTempoReal(req, res) {
+//     const limite_linhas = 7;
 
-    var idAquario = req.params.idAquario;
+//     var idAquario = req.params.idAquario;
 
-    console.log(`Recuperando medidas em tempo real`);
+//     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
-    });
-}
+//     medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+//         if (resultado.length > 0) {
+//             res.status(200).json(resultado);
+//         } else {
+//             res.status(204).send("Nenhum resultado encontrado!")
+//         }
+//     }).catch(function (erro) {
+//         console.log(erro);
+//         console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+//         res.status(500).json(erro.sqlMessage);
+//     });
+// }
+
+
+// function buscarMedidasEmTempoReal(req, res) {
+
+//     var idAquario = req.params.idAquario;
+
+//     console.log(`Recuperando medidas em tempo real`);
+
+//     medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
+//         if (resultado.length > 0) {
+//             res.status(200).json(resultado);
+//         } else {
+//             res.status(204).send("Nenhum resultado encontrado!")
+//         }
+//     }).catch(function (erro) {
+//         console.log(erro);
+//         console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+//         res.status(500).json(erro.sqlMessage);
+//     });
+// }
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    // buscarUltimasMedidas,
+    // buscarMedidasEmTempoReal
 
 }
